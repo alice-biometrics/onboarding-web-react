24b34deb65f8a530baff59ba20c1b3ce95cdae2c
Fran García <fgarcia@alicebiometrics.com>
2021-06-02 18:20:04 +0200

    Upgrade aliceonboarding to 4.4.2
    Also:
    Add example using a basic form
    Add routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import KYC from './KYC';
+import { Switch, Route } from 'react-router-dom'
+
+import KYCForm from './KYCForm';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <KYC></KYC>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/basic' component={KYC}/>
+        <Route path='/form' component={KYCForm}/>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
