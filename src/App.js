585bf2b693148559942a687c806be0c9d8cc434f
Fran García Salomón <frangarciasalomon@gmail.com>
2023-07-20 15:49:21 +0200

    chore: try to reproduce doc detection exception
    Don't use views, use commands instead of normal flow, create and upload front doc in loop for testing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import './App.css';
 import KYC from './KYC';
 import { Switch, Route } from 'react-router-dom'
 
-import KYCForm from './KYCForm';
-import Home from './Home';
-
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/basic' component={KYC}/>
-        <Route path='/form' component={KYCForm}/>
+        <Route exact path='/' component={KYC}/>
       </Switch>
     </div>
   );
--- src/KYC.js
+++ src/KYC.js
@@ -2,9 +2,8 @@ import "aliceonboarding/dist/aliceonboarding.css";
 
 import {
   DocumentType,
-  Onboarding,
+  OnboardingCommands,
   OnboardingConfig,
-  SelfieStageConfig,
   TrialAuthenticator,
 } from "aliceonboarding";
 
@@ -15,6 +14,8 @@ class KYC extends React.Component {
   state = {
     externalData: null,
   };
+
+  onboardingCommands = null;
 
   restartAliceKYC() {
     this.props.history.push("/");
@@ -35,26 +36,87 @@ class KYC extends React.Component {
     this.restartAliceKYC();
   }
 
+  async initOnboardingCommands(threadUserToken, commandsSelector) {
+    if (!this.onboardingCommands) {
+      console.log("Init onboarding commands");
+      const config = new OnboardingConfig().withTheme(
+        "corporate",
+        "minimal",
+        "circle",
+        {
+          alice_color_primary: "#5c88da",
+          alice_color_accent: "black",
+          alice_color_secondary: "#666",
+          alice_color_background: "white",
+          alice_color_check: "#3db387",
+          alice_color_feedback: "#3db387",
+          alice_font_family: "Museo Sans, system-ui, -apple-ui, sans-serif;",
+          alice_title_font_family:
+            "Museo Sans, system-ui, -apple-ui, sans-serif;",
+          alice_modal_title_font_size: "17px",
+          alice_modal_title_font_size_desktop: "20px",
+          alice_button_font_size: "13px",
+          alice_button_font_size_desktop: "14px",
+          alice_button_font_color: "#ffffff",
+          alice_button_border_radius: "8px",
+          alice_button_height: "36px",
+        }
+      );
+
+      this.onboardingCommands = await OnboardingCommands.makeOnboardingCommands(
+        commandsSelector,
+        threadUserToken,
+        config
+      );
+      console.log("Commands initialized");
+    }
+    this.onboardingCommands.createDocument(
+      DocumentType.IDCARD,
+      "ESP",
+      (result) => {
+        console.log(result.document_id);
+        this.onboardingCommands.addDocument(
+          () => {
+            console.log("Document side uploaded");
+            this.startKYC();
+          },
+          (error) => {
+            console.error(error);
+          },
+          (cancel) => {
+            console.error(cancel);
+          },
+          result.document_id,
+          DocumentType.IDCARD,
+          "ESP",
+          "front"
+        );
+      },
+      (error) => {
+        console.error(error);
+      },
+      (cancel) => {
+        console.error(cancel);
+      }
+    );
+  }
+
   startKYC() {
+    console.log("Start KYC");
     const trialToken = process.env.REACT_APP_TRIAL_TOKEN;
     const userEmail = `${Math.random().toString(36).substring(7)}@host.com`;
     let authenticator = new TrialAuthenticator(trialToken, {
       email: userEmail,
     });
-    authenticator.execute().then((userToken) => {
-      console.log("Authentication was successful");
-      let config = new OnboardingConfig()
-        .withUserToken(userToken)
-        .withAddSelfieStage(new SelfieStageConfig())
-        .withAddDocumentStage(DocumentType.IDCARD);
-      new Onboarding("alice", config).run(
-        this.onSuccess.bind(this),
-        this.onFailure.bind(this),
-        this.onCancel.bind(this)
-      );
-    }).catch(error=>{
-      console.log("Error on authentication: " + error);
-    });
+    authenticator
+      .execute()
+      .then((userToken) => {
+        console.log("Authentication was successful");
+        this.initOnboardingCommands(userToken, "alice");
+      })
+      .catch((error) => {
+        console.log("Error on authentication: " + error);
+      });
   }
 
   componentDidMount() {
@@ -69,7 +131,7 @@ class KYC extends React.Component {
 
   render() {
     const container = {
-      paddingTop : "5rem"
+      paddingTop: "5rem",
     };
     return (
       <div style={container}>
